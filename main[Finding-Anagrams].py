7378874ecb4c7636da3063b2f64ae76bd1c1ad96
Bamo <105291706+bamolala@users.noreply.github.com>
2022-06-01 09:24:53 +0100

    Add files via upload

---
+++ main[Finding-Anagrams].py
@@ -0,0 +1,12 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    word = ("hello")
+    word != ("check")
+
+    return True
